9052979254403a41e252d5eb59ef1f4b11b7e853
Omer <omerfarukdasar@gmail.com>
2022-02-16 22:44:35 +0100

    Checkbox revisioned

--- app.js
+++ app.js
@@ -10,37 +10,52 @@ let contents = document.getElementsByClassName("content");
 let inputValue = inputbox.value;
 let checkboxTicks = document.getElementsByClassName("checkmark");
 
-function addingTodo(inputValue) {
-  let eachTodo = document.createElement("div");
-  let toDivision = document.createElement("div");
 
+// This functions add todo to todosContainer
+function addingTodo(inputValue) {
+
+  let eachTodo = document.createElement("div");
   let inclusiveButton = document.createElement("div");
   let remButton = document.createElement("button");
-
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   let checkboxDiv = document.createElement("div");
 
   if (inputValue.length > 3) {
-    // checkbox
+
+    let toDivision = document.createElement("div");
+
+    // checkbox creating
     checkbox.classList.add("checkMarx");
     checkboxDiv.classList.add("checkmark");
     checkboxDiv.appendChild(checkbox);
     toDivision.appendChild(checkboxDiv);
 
-    // content
+    // content creating
     eachTodo.classList.add("content");
     toDivision.classList.add("todosDiv");
     eachTodo.innerText = inputValue;
     toDivision.appendChild(eachTodo);
 
-    // button
+    // checkbox event
+    checkbox.addEventListener("click", (e) => {
+      if (e.target.checked) {
+        eachTodo.classList.add("line-through");
+      } else {
+        eachTodo.classList.remove("line-through");
+      }
+    });
+
+    // remove button
     remButton.classList.add("removeButton", "button-62");
     inclusiveButton.classList.add("buttonOuter");
     remButton.innerText = "X";
     toDivision.appendChild(remButton);
 
     todosContainer.appendChild(toDivision);
+    remButton.addEventListener("click", () => {
+      toDivision.parentNode.removeChild(toDivision);
+    });
   }
 }
 
@@ -50,24 +65,6 @@ addButton.addEventListener("click", () => {
   addingTodo(toDoText);
   if (inputbox.value.length > 3) {
     inputbox.value = "";
-  }
-  // overline the todos
-  for (let i = 0; i < checkboxTicks.length; i++) {
-    checkboxTicks[i].addEventListener("change", (e) => {
-      if (e.target.checked) {
-        contents[i].classList.add("line-through");
-      } else {
-        contents[i].classList.remove("line-through");
-      }
-    });
-  }
-
-  // deleting buttons
-  for (let i = 0; i < removeButtons.length; i++) {
-    removeButtons[i].addEventListener("click", (e) => {
-      todosContainer.removeChild(e.target.parentElement);
-      removeButtons.pop(i);
-    });
   }
 });
 
@@ -79,22 +76,5 @@ inputbox.addEventListener("keypress", function (e) {
       inputbox.value = "";
     }
     // overline the todos
-    for (let i = 0; i < checkboxTicks.length; i++) {
-      checkboxTicks[i].addEventListener("change", (e) => {
-        if (e.target.checked) {
-          contents[i].classList.add("line-through");
-        } else {
-          contents[i].classList.remove("line-through");
-        }
-      });
-    }
-
-    // deleting buttons
-    for (let i = 0; i < removeButtons.length; i++) {
-      removeButtons[i].addEventListener("click", (e) => {
-        todosContainer.removeChild(removeButtons[i].parentElement);
-        removeButtons.pop(i);
-      });
-    }
   }
 });
